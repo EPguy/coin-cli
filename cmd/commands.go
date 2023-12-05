f4f757d198d7c4e7b319174bae50471a4587b71c
kjs <k021229@naver.com>
2023-12-05 10:32:15 +0900

    added more information of the list command

--- cmd/commands.go
+++ cmd/commands.go
@@ -2,10 +2,13 @@ package cmd
 
 import (
 	"fmt"
+	"math"
 	"strings"
 
 	gh "github.com/EPguy/coin-cli/github"
 	"github.com/spf13/cobra"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
 )
 
 var Price = &cobra.Command{
@@ -14,26 +17,33 @@ var Price = &cobra.Command{
 	Long:  `coin-cli price <COIN_SYMBOL>`,
 	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
+		p := message.NewPrinter(language.English)
 		searchVal := strings.Join(args, " ")
 		ticker := gh.SearchTicker(searchVal)
-		fmt.Printf("%s price(USD) is : %f", searchVal, *ticker.Quotes["USD"].Price)
+		p.Printf("%s price(USD) is : %f", searchVal, *ticker.Quotes["USD"].Price)
 	},
 }
 
 var List = &cobra.Command{
 	Use:   "list",
-	Short: "List coins",
+	Short: "List coins Top 100",
 	Long:  `coin-cli list`,
 	Args:  cobra.MinimumNArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
-		tickers := gh.SearchTickerList()
+		p := message.NewPrinter(language.English)
+		tickers := gh.SearchTickerList()[:100]
 		for _, ticker := range tickers {
-			fmt.Printf("coin id : %s\n", *ticker.ID)
-			fmt.Printf("coin name : %s\n", *ticker.Name)
-			fmt.Printf("coin symbol : %s\n", *ticker.Symbol)
-			fmt.Printf("coin price(USD) : %f\n", *ticker.Quotes["USD"].Price)
-			fmt.Printf("coin max supply : %d\n", *ticker.MaxSupply)
-			fmt.Printf("coin total supply : %d\n", *ticker.TotalSupply)
+			p.Printf("coin rank : %d\n", *ticker.Rank)
+			p.Printf("coin id : %s\n", *ticker.ID)
+			p.Printf("coin name : %s\n", *ticker.Name)
+			p.Printf("coin symbol : %s\n", *ticker.Symbol)
+			p.Printf("coin price : %f$\n", *ticker.Quotes["USD"].Price)
+			p.Printf("coin max supply : %d\n", *ticker.MaxSupply)
+			p.Printf("coin total supply : %d\n", *ticker.TotalSupply)
+			p.Printf("coin market cap : %.0f$\n", math.Floor(*ticker.Quotes["USD"].MarketCap))
+			p.Printf("percent change(24h) : %.2f%%\n", *ticker.Quotes["USD"].PercentChange24h)
+			p.Printf("percent change(7d) : %.2f%%\n", *ticker.Quotes["USD"].PercentChange7d)
+			p.Printf("percent change(30d) : %.2f%%\n", *ticker.Quotes["USD"].PercentChange30d)
 
 			fmt.Printf("-----------------------------------------------------\n")
 		}
